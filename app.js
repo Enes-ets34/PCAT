c933c51c47f08a05ca9ee73994abe1bb7fc5f427
enes taha sarı <ets.34.es@gmail.com>
2021-10-05 17:08:09 +0300

    middleware

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("sa");
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// MIDDLEWARES
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  app.sendFile(path.resolve(__dirname, "/public/index.html"));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda çalışıyor...`);
+});
