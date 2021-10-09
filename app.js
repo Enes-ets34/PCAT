88edbd2e49bbd21105a1587191eaaed8433cd87b
enes taha sarı <ets.34.es@gmail.com>
2021-10-09 14:54:04 +0300

    file upload

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const fileUpload = require("express-fileupload");
 const Photo = require("./models/Photo");
+const fs = require("fs");
 const app = express();
 const ejs = require("ejs");
 const path = require("path");
@@ -15,6 +17,7 @@ mongoose.connect("mongodb://localhost/pcat-test-db");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 
 // ROUTES
 app.get("/", async (req, res) => {
@@ -26,19 +29,36 @@ app.get("/", async (req, res) => {
 app.get("/about", (req, res) => {
   res.render("about");
 });
+
+// Add Page
 app.get("/add", (req, res) => {
   res.render("add");
 });
+
+// Add  Photo
 app.post("/photos", async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect("/");
+  const uploadDir = "public/uploads";
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+  let uploadedImage = req.files.image;
+  let uploadPath = __dirname + "/public/uploads/" + uploadedImage.name;
+  uploadedImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: "/uploads/" + uploadedImage.name,
+    });
+    res.redirect("/");
+  });
 });
-app.get("/photos/:id",async (req, res) => {
+
+// Get Single Photo
+app.get("/photos/:id", async (req, res) => {
   const photo = await Photo.findById(req.params.id);
   console.log("photo :>> ", {
-   photo
+    photo,
   });
-  res.render("photo",{photo});
+  res.render("photo", { photo });
 });
 
 const port = 3000;
